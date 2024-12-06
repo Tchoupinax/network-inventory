177c1a6ccf30da551193deb2060506e056311551
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-06 09:22:49 +0100

    feat: add table view

---
+++ types/network.ts
@@ -0,0 +1,6 @@
+export type Network = {
+  name: string;
+  cidr: string; // "10.10.10.10/16" — https://cidr.xyz
+  color: string;
+  devices?: Array<{ name: string; ip: string; color: string }>;
+};
